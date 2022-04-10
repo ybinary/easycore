1ed8ce79f736aad86f5ca683b0a6e22f4a70eaa0
yangcheng <874349119@qq.com>
2022-04-10 19:19:36 +0800

    test

---
+++ easycore/src/main/java/com/business/easycore/vmodel/MBaseVModel.kt
@@ -0,0 +1,17 @@
+package com.business.easycore.vmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+
+class MBaseVModel : ViewModel() {
+
+    private var mContext: Context? = null
+
+    fun withContext(){
+
+
+    }
+
+
+
+}
